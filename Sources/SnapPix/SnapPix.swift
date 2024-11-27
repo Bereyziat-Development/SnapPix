939b7bb2d9fc4d7bc7d5534c55bba6c7826f6205
Szymon Wnuk <szwnuk1@gmail.com>
2024-11-27 15:56:24 +0100

    fix: postiioning of the error size text

--- Sources/SnapPix/SnapPix.swift
+++ Sources/SnapPix/SnapPix.swift
@@ -137,9 +137,11 @@ public struct SnapPix<
                     }
                 }
             }
-                    Text(isShowingFileSizeAlert ? "The selected file is bigger than \(maxFileSizeB / 1024000)MB." : "")
+            
+            
+                    Text( "The selected file is bigger than \(maxFileSizeB / 1024000)MB.")
                                .font(.system(size: 16, weight: .regular))
-                               .foregroundColor(.red)
+                               .foregroundColor(isShowingFileSizeAlert ? .red : .clear)
                                .padding(.horizontal)
                                .animation(.easeInOut)
             
